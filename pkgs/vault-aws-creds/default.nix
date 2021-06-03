2ac3a5250df0398e76c61b4a81ad4982cdfcb7ef
Patrick Aikens <paikens@gmail.com>
2021-06-03 12:42:57 -0400

    Add version to vault-aws-creds

--- pkgs/vault-aws-creds/default.nix
+++ pkgs/vault-aws-creds/default.nix
@@ -1,22 +1,25 @@
 { lib, stdenv, fetchFromGitHub }:
+let
+  version = "0.2.9";
+in
+  stdenv.mkDerivation {
+    inherit version;
+    name = "vault-aws-creds";
+    src = fetchFromGitHub {
+      owner = "jantman";
+      repo = "vault-aws-creds";
+      rev = "338a1d2b4b653c83452de4842a4bf811958a94a3";
+      sha256 = "116q9ya4z70vcjhb6ibrdzv8prh8kk9adhwf3blffs4h2xrjvixb";
+    };
+    installPhase = ''
+      mkdir -p $out/bin $out/share/vault-aws-creds
+      cp $src/*.py $out/share/vault-aws-creds
 
-stdenv.mkDerivation {
-  name = "vault-aws-creds";
-  src = fetchFromGitHub {
-    owner = "jantman";
-    repo = "vault-aws-creds";
-    rev = "master";
-    sha256 = "116q9ya4z70vcjhb6ibrdzv8prh8kk9adhwf3blffs4h2xrjvixb";
-  };
-  installPhase = ''
-    mkdir -p $out/bin $out/share/vault-aws-creds
-    cp $src/*.py $out/share/vault-aws-creds
-
-    cat > $out/bin/aws-console <<EOF
-    #! /bin/bash
-    open \$($out/share/vault-aws-creds/aws-sts-console-url.py)
-    EOF
-    chmod +x $out/bin/aws-console
-  '';
-}
+      cat > $out/bin/aws-console <<EOF
+      #! /bin/bash
+      open \$($out/share/vault-aws-creds/aws-sts-console-url.py)
+      EOF
+      chmod +x $out/bin/aws-console
+    '';
+  }
 
