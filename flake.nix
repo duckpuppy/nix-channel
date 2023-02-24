91c8a9eb88cd3e7f07da6b956d49f64689b194c1
Patrick Aikens <paikens@gmail.com>
2023-02-24 12:01:06 -0500

    Add templates to root flake.nix

--- flake.nix
+++ flake.nix
@@ -2,20 +2,32 @@
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
-  let
-    systems = [
-      "x86_64-linux"
-      "i686-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "armv6l-linux"
-      "armv7l-linux"
-    ];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-  in
-  {
-    packages = forAllSystems (system: import ./default.nix {
-      pkgs = import nixpkgs { inherit system; };
-    });
-  };
+    let
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "armv6l-linux"
+        "armv7l-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems (system: import ./default.nix {
+        pkgs = import nixpkgs { inherit system; };
+      });
+
+      templates =
+        let
+          devenv-simple = {
+            path = ./templates/devenv-simple;
+            description = "A basic direnv supported Nix flake with devenv integration.";
+          };
+        in
+        {
+          inherit devenv-simple;
+          default = devenv-simple;
+        };
+    };
 }
