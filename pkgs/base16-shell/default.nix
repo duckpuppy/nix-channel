436dc1c755d44fa37490e280f9ba02f3fc29fd9c
Patrick Aikens <paikens@gmail.com>
2023-05-08 10:50:30 -0400

    feat: Add catppuccin themes to base16-shell

--- pkgs/base16-shell/default.nix
+++ pkgs/base16-shell/default.nix
@@ -2,12 +2,14 @@
 
 let
   base16-shell-src = sources.base16-shell;
+  extra-themes-src = ./themes;
 in
 stdenv.mkDerivation {
   name = "base16-shell";
   src = base16-shell-src;
   installPhase = ''
-     mkdir -p $out/share/base16-shell
-     cp -r * $out/share/base16-shell/
+    mkdir -p $out/share/base16-shell
+    cp -r * $out/share/base16-shell/
+    cp -r ${extra-themes-src}/* $out/share/base16-shell/scripts
   '';
 }
