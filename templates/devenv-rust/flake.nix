48b00593dc945ccc5cff2f087cd49e4824c36c6b
Patrick Aikens <paikens@gmail.com>
2023-07-19 13:10:03 -0400

    fix: Use flake-parts for rust template

--- templates/devenv-rust/flake.nix
+++ templates/devenv-rust/flake.nix
@@ -3,53 +3,44 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Updated Rust toolchains
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, devenv, systems, fenix, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem
-        (system:
-          let
-            # pkgs = nixpkgs.legacyPackages.${system};
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [
-                fenix.overlays.default
-              ];
-            };
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [
-                    git
-                    zig
-                    cargo-zigbuild
-                  ];
+  outputs = { self, nixpkgs, flake-parts, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = import inputs.systems;
 
-                  # https://devenv.sh/languages/
-                  languages.rust.enable = true;
-                  languages.rust.channel = "stable";
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          # https://devenv.sh/reference/options/
+          packages = with pkgs; [
+            git
+            zig
+            cargo-zigbuild
+          ];
 
-                  # https://devenv.sh/pre-commit-hooks/
-                  pre-commit.hooks.nixfmt.enable = true;
-                  pre-commit.hooks.cargo-check.enable = true;
-                  pre-commit.hooks.clippy.enable = true;
-                  pre-commit.hooks.convco.enable = true;
-                  pre-commit.hooks.rustfmt.enable = true;
-                }
-              ];
-            };
-          });
+          devcontainer.enable = true;
+
+          # https://devenv.sh/languages/
+          languages.rust.enable = true;
+          languages.rust.channel = "stable";
+
+          # https://devenv.sh/pre-commit-hooks/
+          pre-commit.hooks.nixfmt.enable = true;
+          pre-commit.hooks.cargo-check.enable = true;
+          pre-commit.hooks.clippy.enable = true;
+          pre-commit.hooks.convco.enable = true;
+          pre-commit.hooks.rustfmt.enable = true;
+        };
+      };
     };
 }
